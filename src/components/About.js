5081a86d449fc6b92540c5f0ffbc0eb8218fc40c
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2023-08-31 17:10:39 +0200

    r

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
 // import john from './images/class.jpeg'
 import classmates from './images/class.jpeg'
 import './About.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const About = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
         <div className='about' id='about'>
             <div className='container'>
                 <img id="classimg" src={classmates} alt="" />
-                <div className='col-2'>
+                <div className='col-2'  data-aos="fade-down"
+
+                >
                     <h2>About</h2>
                     <span className='line'></span>
-                    <p>Intense is an International Financial Planning company with offices i n multiple jurisdictions over the world. Working with Intense gives me the ability to advise internat ional expatriates living in the middle east from where I live in USA.</p>
-                    <p>I am John Doe, a senior advisor for an independently owned financial planning company called Intense.</p>
-                    <button className='button'>Explore More</button>
+                    <p>Over the mast four years, I was a Code Instructor at intec Brussel
+                        I transitioned from an Instructor to Head Instructor, specializing in web development for adults, often with university-level IT backgrounds. I covered GIT, HTML, CSS, JavaScript, React, PHP, Laravel, and Databases.
+                        <br />
+                        <br />
+                        I not only guided students in debugging exercises but also coached and motivated them, along with fellow instructors. Additionally, I played a pivotal role in establishing the Intec website.
+                        <br />
+                        <br />
+
+                        Before I was a graphic designer for over 25 years, I bring a multilingual aspect, fluently speaking/writing four languages (NL-FR-EN-ES (DE) ). As I find more joy in coding I changed my carreer in 2017 to become a full stack developer.
+
+                    </p>
+
+                    <button className='button'>Download reference letter previous employer</button>
                 </div>
             </div>
         </div>
