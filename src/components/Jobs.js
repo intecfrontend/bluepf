61f9356b99798f373018147f0511c26aff29926d
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2023-09-02 12:23:02 +0200

    pink modal

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import github from './myjobs/GitHub-Mark.png'
 import reactL from './myjobs/amazonpf/react.png'
 import reduxL from './myjobs/amazonpf/redux.png'
@@ -10,14 +10,46 @@ import './Testimonials.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './Jobs.css'
+import './Appmodal.css'
+import { createPortal } from "react-dom";
+import {Modal} from "./modalAmazon/Modal";
 
 
 const Jobs = () => {
+
+    const [modalOpen, setModalOpen] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const handleButtonClick = (value) => {
+        setModalOpen(false);
+        setMessage(value);
+    };
+
     useEffect(() => {
         AOS.init();
     }, [])
     return (
         <div className='jobs' id='jobs'>
+
+            {message}
+            <button className="btn btn-open" onClick={() => setModalOpen(true)}>
+                Open
+            </button>
+            {modalOpen &&
+                createPortal(
+                    <Modal
+                        closeModal={handleButtonClick}
+                        onSubmit={handleButtonClick}
+                        onCancel={handleButtonClick}
+                    >
+                        <h1>This is a modal</h1>
+                        <br />
+                        <p>This is the modal description</p>
+                    </Modal>,
+                    document.body
+                )}
+
+
             <div className='container'>
                 <h2 id="whitereal" >Realisations</h2>
                 <span className='line'></span>
@@ -27,6 +59,7 @@ const Jobs = () => {
                             <img className="cardjob" id="catimg" src={github} alt='github' />
                         </a>
                         <p>
+                            <h1>About This Site</h1>
                             <span >Please, click on the GitHub Logo (cat)<br />
                                 <a target="_blank" className="catref" href="https://github.com/intecfrontend/bluepf"> to see the code of this site. </a>
                             </span>
@@ -36,24 +69,24 @@ const Jobs = () => {
                         </p>
                         <p>
                             <span >
-                                <a target="_blank" className="catref" href="https://github.com/intecfrontend/bluepf">  <button className="catbtn"> More info </button> </a>
+                                <a target="_blank" className="catref" href="https://github.com/intecfrontend/bluepf">  <button className="catbtn btn btn-open"> More info </button> </a>
                             </span>
                         </p>
                     </div>
                     <div className='card'>
                         <img className="cardjob" src={reactL} alt='reduxL' />
                         <img className="cardjob" src={reduxL} alt='reduxL' />
-                        <img className="cardfill" id="amazonlogin" src={amazonlogin} alt='reduxL' />                        
+                        <img className="cardfill" id="amazonlogin" src={amazonlogin} alt='reduxL' />
                         <p>
                             <span >
-                                <a target="_blank" className="catref" href="https://github.com/intecfrontend/bluepf">  <button className="catbtn"> More info </button> </a>
+                                <button className="catbtn btn btn-open" onClick={() => setModalOpen(true)}> More info     </button>
                             </span>
                         </p>
 
                     </div>
                     <div className='card'>
                         <img className="cardjob" src={laraL} alt='laraL' />
-                        <img className="cardfill" id="laralogin" src={posts} alt='reduxL' />                        
+                        <img className="cardfill" id="laralogin" src={posts} alt='reduxL' />
                         <p>
                             <span >
                                 <a target="_blank" className="catref" href="https://github.com/intecfrontend/bluepf">  <button className="catbtn"> More info </button> </a>
